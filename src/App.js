d5c86698d73e893042657c379da91def5b8dc032
lqy57 <larryyang91@gmail.com>
2023-03-27 12:32:41 -0500

    frontend connect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [homes, setHomes] = useState([])
+
+  const getHomes = () => {
+    axios.get('http://localhost:8000/homes')
+    .then((response) => setHomes(response.data), 
+    (err) => console.log(err))
+    .catch((error) => console.log(error))
+  }
+
+  useEffect(()=>{
+    getHomes()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }

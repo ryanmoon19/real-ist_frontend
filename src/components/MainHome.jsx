f300570b853aadd1ba671eb4db9f48d464647100
rominadouk <douk.romina@gmail.com>
2023-03-28 16:22:43 -0700

    added a dolla sign

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -20,7 +20,7 @@ const MainHome = () => {
         <>
             <div className= "flex flex-col items-center justify-center w-screen h-96 bg-[url('https://www.livebuyers.com/wp-content/uploads/2020/02/Los-Angeles-CA.jpg')] bg-cover bg-no-repeat bg-center bg-fixed">
             <p className="text-[70px] text-white my-0 mx-auto text-shadow-lg">Let us take you home.</p>
-            <input className="my-0 text-black border border-gray-400 rounded-md w-96 px-4 py-3 mt-4"type="text" placeholder="Search a Zipcode or State"/>
+            <input className="my-0 text-black border border-gray-400 rounded-md w-96 px-4 py-3 mt-4"type="text" placeholder="Search a City, ZIP Code, or State"/>
             </div>
             <div className="mt-10 ml-20">
                 <h3 className="text-[25px]">Homes For You</h3>
@@ -31,7 +31,7 @@ const MainHome = () => {
                     return (
                         <div className="relative flex" key={home.id}>
                             <img className="w-full" src={home.image} alt={home.description}/>
-                            <p className="absolute bottom-5 left-0 z-10 p-4 text-white text-[20px]"><strong>{home.price}</strong></p>
+                            <p className="absolute bottom-5 left-0 z-10 p-4 text-white text-[20px]"><strong>${home.price}</strong></p>
                             <p className="absolute bottom-0 left-0 z-10 p-4 text-white">{home.houseNum} {home.street} {home.city}, {home.state}</p>
                         </div>
                     )

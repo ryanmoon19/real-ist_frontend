d2466bdea4a3179453c91a41d2e44d397b58c4e9
ryanmoon19 <ryanhmoon11@gmail.com>
2023-03-28 16:20:28 -0700

    styling for sell 80% done

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -42,55 +42,56 @@ const Create = (props) => {
 
 
     return (
-        <>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="houseNum">House Number: </label>
-            <input type="text" name="houseNum" onChange = {handleChange}/>
-            <br />
-            <br />
-            <label htmlFor="street">Street: </label>
-            <input type="text" name="street" onChange={handleChange}/>
-            <br/>
-            <br/>
-            <label htmlFor="city">City:</label>
-            <input type="text" name="city" onChange={handleChange}/>
-            <br/>
-            <br/>
-            <label htmlFor="state">State:</label>
-            <input type="text" name="state" onChange={handleChange}/>
-            <br/>
-            <br/>
-            <label htmlFor="zip">Zip:</label>
-            <input type="number" name="zip" onChange={handleChange} />
-            <br/>
-            <br/>
-            <label htmlFor="price">Price:</label>
-            <input type="number" name="price" onChange={handleChange} />
-            <br/>
-            <br/>
-            <label htmlFor="bedroom"># of Bedrooms:</label>
-            <input type="number" name="bedroom" onChange={handleChange} />
-            <br/>
-            <br/>
-            <label htmlFor="bathroom"># of Bathrooms:</label>
-            <input type="number" name="bathroom" onChange={handleChange} />
-            <br/>
-            <br/>
-            <label htmlFor="description">Description:</label>
-            <input type="text" name="description" onChange={handleChange} />
-            <br/>
-            <br/>
-            <label htmlFor="squareFeet">Square Feet:</label>
-            <input type="number" name="squareFeet" onChange={handleChange} />
-            <br/>
-            <br/>
-            <label htmlFor="image">Image:</label>
-            <input type="text" name="image" onChange={handleChange} />
-            <br/>
-            <br/>
-            <input type="submit" onChange={handleChange} value="Add Home"/>
-        </form>
-    </>
-      );
+        <div className='mt-4 mx-auto w-8/12 py-8 px-6 shadow-xl rounded-lg sm:px-10 '>
+            <h1 className='mb-3 block text-xl font-medium text-gray-700 text-center'>Add A New Listing</h1>
+            <form className="bg-white py-8 px-6 shadow-lg rounded-lg sm:px-10" onSubmit={handleSubmit}>
+                <label className='block text-xl font-medium text-gray-700' htmlFor="houseNum">House Number: </label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' name="houseNum" onChange = {handleChange}/>
+                <br />
+                <br />
+                <label className='block text-xl font-medium text-gray-700' htmlFor="street">Street: </label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' type="text" name="street" onChange={handleChange}/>
+                <br/>
+                <br/>
+                <label className='block text-xl font-medium text-gray-700' htmlFor="city">City:</label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' type="text" name="city" onChange={handleChange}/>
+                <br/>
+                <br/>
+                <label className='block text-xl font-medium text-gray-700' htmlFor="state">State:</label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' type="text" name="state" onChange={handleChange}/>
+                <br/>
+                <br/>
+                <label className='block text-xl font-medium text-gray-700' htmlFor="zip">Zip:</label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' type="number" name="zip" onChange={handleChange} />
+                <br/>
+                <br/>
+                <label className='block text-xl font-medium text-gray-700' htmlFor="price">Price:</label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' type="number" name="price" onChange={handleChange} />
+                <br/>
+                <br/>
+                <label className='block text-xl font-medium text-gray-700' htmlFor="bedroom"># of Bedrooms:</label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' type="number" name="bedroom" onChange={handleChange} />
+                <br/>
+                <br/>
+                <label className='block text-xl font-medium text-gray-700' htmlFor="bathroom"># of Bathrooms:</label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' type="number" name="bathroom" onChange={handleChange} />
+                <br/>
+                <br/>
+                <label className='block text-xl font-medium text-gray-700' htmlFor="description">Description:</label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' type="text" name="description" onChange={handleChange} />
+                <br/>
+                <br/>
+                <label className='block text-xl font-medium text-gray-700' htmlFor="squareFeet">Square Feet:</label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' type="number" name="squareFeet" onChange={handleChange} />
+                <br/>
+                <br/>
+                <label className='block text-xl font-medium text-gray-700' htmlFor="image">Image:</label>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600' type="text" name="image" onChange={handleChange} />
+                <br/>
+                <br/>
+                <input className='w-full border border-gray-400 px-3 py-1 rounded-md shadow-lg focus:outline-none focus:border-blue-600 focus:ring-1 focus:ring-blue-300' type="submit" onChange={handleChange} value="Add Home"/>
+            </form>
+        </div>
+    );
     }
 export default Create;

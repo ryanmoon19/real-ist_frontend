f38f78c13cd1d83c43ecd16495865aa7abba5e9e
rominadouk <douk.romina@gmail.com>
2023-03-29 16:33:46 -0700

    completed functioning search bar, search runs when user presses enter key

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -4,7 +4,18 @@ import axios from 'axios'
 
 const MainHome = () => {
 
-    const [homes, setHomes] = useState([]);
+    const [homes, setHomes]= useState([]);
+    const [search, setSearch]= useState("")
+    const [searchField, setSearchField] = useState("")
+
+    const handleChange = (event) => {
+        setSearchField(event.target.value)
+    }
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            setSearch(searchField)
+        }
+    }
 
     const getOneHome = (id) => {
         return axios.get(`http://localhost:8000/homes/${id}`)
@@ -30,22 +41,33 @@ const MainHome = () => {
     return(
         <>
             <div className= "flex flex-col items-center justify-center w-screen h-96 bg-[url('https://www.livebuyers.com/wp-content/uploads/2020/02/Los-Angeles-CA.jpg')] bg-cover bg-no-repeat bg-center bg-fixed">
-            <p className="sm:text-[70px] text-[50px] text-white my-0 mx-auto">Let us take you home.</p>
-            <input className="my-0 text-black border border-gray-400 rounded-md w-96 px-4 py-3 mt-4"type="text" placeholder="Search a City, ZIP Code, or State"/>
+                <p className="sm:text-[70px] text-[50px] text-white my-0 mx-auto">Let us take you home.</p>
+                <input className="my-0 text-black border border-gray-400 rounded-md w-96 px-4 py-3 mt-4"type="text" placeholder="Search a City, ZIP Code, or State" onChange={handleChange} onKeyDown={handleKeyDown}/>
             </div>
             <div className="mt-10">
                 <h3 className="text-[25px] text-center">Homes For You</h3>
                 <p className="text-[15px] text-center">Current Location Placeholder</p>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-5 mx-auto max-w-[1700px]">
-                {homes.map((home)=>{
+                {homes.filter((home)=> {
+                    if(search === ""){
+                        return home
+                    } else if(home.city.toLowerCase().includes(search.toLowerCase())) {
+                        return home
+                    } else if(home.zip.toString().includes(search.toString())){
+                        return home
+                    } else if(home.state.toLowerCase().includes(search.toLowerCase())) {
+                        return home
+                    }
+                })
+                .map((home)=>{
                     return (                
                     <div className="relative flex" key={home.id}>
                         <img className="w-full h-auto" src={home.image} alt={home.description}/>
                         <Link to={`/oneHome/${home.id}`} onClick={() => getOneHome(home.id)}>
                             <div className="absolute inset-0 flex flex-col justify-end px-4 py-6">
                                 <p className="text-white text-xl mb-2"><strong>${home.price}</strong></p>
-                                <p className="text-white">{home.houseNum} {home.street} {home.city}, {home.state}</p>
+                                <p className="text-white">{home.houseNum} {home.street} {home.city}, {home.state} {home.zip}</p>
                             </div>
                         </Link>
                     </div>
